bbf090c04592f1edf2be236f0335543dfa29013f
VR <33652017+vmfvvs@users.noreply.github.com>
2019-11-01 17:13:25 +0300

    Add files via upload

---
+++ Aerospike_DE.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*
+from __future__ import print_function
+import aerospike
+from aerospike  import predicates as p
+import logging
+import pprint
+import sys
+
+try:
+    config = {
+        'hosts': [ ('127.0.0.1', 3000) ]
+    }
+    client = aerospike.client(config).connect()
+except Exception as e:
+    print("error: {0}".format(e), file=sys.stderr)
+    sys.exit(1)
+
+def add_customer(customer_id, phone_number, lifetime_value):
+  key = ('test', 'demo', customer_id)
+  try:
+    client.put(key, {
+    'phone': phone_number, 
+    'ltv': lifetime_value})
+  except Exception as e:
+   import sys
+   print("error: {0}".format(e), file=sys.stderr)
+
+def get_ltv_by_id(customer_id):
+    key = ('test', 'demo', customer_id)
+    (key, metadata, record) = client.get(key)
+    if(record == {}):
+     logging.error('Requested non-existent customer ' + str(customer_id))
+    else:
+    return record.get('ltv')
+
+def get_ltv_by_phone(phone_number):
+ 
+    query = client.query('test', 'demo')   
+    query.select('ltv')
+    query.where(p.equals('phone', phone_number))
+    records = query.results({'total_timeout':2000})
+    return records
+    
+    client.close()
